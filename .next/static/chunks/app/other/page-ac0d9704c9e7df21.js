d8bb784d0594700e1fed0db907ed2ae1e11b4d88
Ratchanon <ratchanonmcnon2015@gmail.com>
2026-05-10 15:45:12 +0700

    chore: untrack .next directory and update .gitignore

--- .next/static/chunks/app/other/page-ac0d9704c9e7df21.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[654,714,582,301,931,928,607],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
